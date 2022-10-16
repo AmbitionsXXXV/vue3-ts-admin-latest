0215fe066b6ac3881b600b427220c77fde954c20
Etcetera <yilonglei790@gmail.com>
2022-10-16 11:15:29 +0800

    充值项目基本样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <h1>ONE OK ROCK & Aimyon</h1>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import "normalize.css"
+import "./assets/css/index.less"
 
 createApp(App).mount("#app")

09f4bf840e57bc0444c276b84e9cd55ca797f490
Etcetera <yilonglei790@gmail.com>
2022-10-17 16:52:34 +0800

    导航守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
+import { localCache } from "@/utils/cache"
 import { createRouter, createWebHashHistory } from "vue-router"
+import { LOGIN_TOKEN } from "@/global/constants"
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -21,6 +23,19 @@ const router = createRouter({
       component: () => import("../views/not-found/NotFound.vue"),
     },
   ],
+})
+
+// 导航守卫
+// 参数: to(跳转到的位置)/from(从哪里跳转过来)
+// 返回值: 返回值决定导航的路径(不返回或者返回undefined, 默认跳转)
+// 举个栗子: / => /main
+// to: /main from: / 返回值: /abc
+router.beforeEach((to) => {
+  // 只有登录成功(token), 才能真正进入到main页面
+  const token = localCache.getCache(LOGIN_TOKEN)
+  if (to.path.startsWith("/main") && !token) {
+    return "/login"
+  }
 })
 
 export default router
--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,9 +1,9 @@
+import { LOGIN_TOKEN } from "@/global/constants"
+import router from "@/router"
 import { accountLoginRequest } from "@/service/login/login"
 import type { IAccount } from "@/types"
 import { localCache } from "@/utils/cache"
 import { defineStore } from "pinia"
-
-const LOGIN_TOKEN = "login/token"
 
 const useLoginStore = defineStore("login", {
   state: () => ({
@@ -21,6 +21,9 @@ const useLoginStore = defineStore("login", {
 
       // 2.进行本地缓存
       localCache.setCache(LOGIN_TOKEN, this.token)
+
+      // 3.进行页面跳转
+      router.push("/main")
     },
   },
 })

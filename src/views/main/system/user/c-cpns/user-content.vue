4c28531293d465ee4f2a41a67a5a6ecb76deec86
Etcetera <yilonglei790@gmail.com>
2022-10-19 20:07:56 +0800

    用户信息渲染

--- src/views/main/system/user/c-cpns/user-content.vue
+++ src/views/main/system/user/c-cpns/user-content.vue
@@ -1,27 +1,71 @@
 <template>
   <div class="content">
-    <dlv class="header">
+    <div class="header">
       <h3 class="title">用户列表</h3>
       <el-button type="primary">新建用户</el-button>
-    </dlv>
+    </div>
     <div class="table">
-      <el-table>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
-        <el-table-column></el-table-column>
+      <el-table :data="usersList" border style="width: 100%">
+        <el-table-column align="center" type="selection" width="50px" />
+        <el-table-column
+          align="center"
+          type="index"
+          label="序号"
+          width="60px"
+        />
+
+        <el-table-column
+          align="center"
+          label="用户名"
+          prop="name"
+          width="150px"
+        />
+        <el-table-column
+          align="center"
+          label="真实姓名"
+          prop="realname"
+          width="150px"
+        />
+        <el-table-column
+          align="center"
+          label="手机号码"
+          prop="cellphone"
+          width="150px"
+        />
+        <el-table-column
+          align="center"
+          label="状态"
+          prop="enable"
+          width="100px"
+        />
+        <el-table-column align="center" label="创建时间" prop="createAt" />
+        <el-table-column align="center" label="更新时间" prop="updateAt" />
+
+        <el-table-column align="center" label="操作" width="150px">
+          <el-button size="small" icon="Edit" type="primary" text>
+            编辑
+          </el-button>
+          <el-button size="small" icon="Delete" type="danger" text>
+            删除
+          </el-button>
+        </el-table-column>
       </el-table>
     </div>
     <div class="pagination">分页</div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from "pinia"
+import useSystemStore from "@/store/main/system/system"
+
+// 1.发起action，请求usersList的数据
+const systemStore = useSystemStore()
+systemStore.postUsersListAction()
+
+// 2.获取usersList数据,进行展示
+const { usersList } = storeToRefs(systemStore)
+</script>
 
 <style lang="less" scoped>
 .content {
